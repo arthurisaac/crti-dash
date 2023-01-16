460b3023f609d8486ce5d5c7d11dfc0dd9f4a03f
odisys <36701919+odisys@users.noreply.github.com>
2023-01-16 08:25:41 +0000

    Dashboard
     Dashboad Css and jsx

--- src/page/Sidebar.js
+++ src/page/Sidebar.js
@@ -3,6 +3,25 @@ import {useEffect, useState} from "react";
 import {onAuthStateChanged} from "firebase/auth";
 import {auth} from "../firebase";
 
+let arrow = document.querySelectorAll(".arrow");
+
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+        let arrowParent = e.target.parentElement.parentElement;
+        console.log(arrowParent);
+        arrowParent.classList.toggle("showMenu");
+    });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+
+// Unsoucis sur le Onclick pour activer le arrow du subMenu
+// sidebarBtn.onclick = function(){
+//     sidebar.classList.toggle("active");
+// }
+// FinOnCilick
+
 export default function Sidebar() {
     const [user, setUser] = useState(null);
 
@@ -14,55 +33,56 @@ export default function Sidebar() {
         })
     })
 
-    const menus = [
-        {
-            name: "Dashboard",
-            url: "/"
-        },
-        {
-            name: "Emplacement GPS",
-            url: "/emplacement-gps"
-        },
-        {
-            name: "Contacts",
-            url: "/contacts"
-        },
-        {
-            name: "Messages textes",
-            url: "/sms"
-        },
-        {
-            name: "Log d'appels",
-            url: "/call-logs"
-        },
-        {
-            name: "Application installées",
-            url: "/installed-apps"
-        },
-        {
-            name: "Key logs",
-            url: "/keylogger"
-        },
-        {
-            name: "Caméra photo",
-            url: "/camera-photo"
-        }
-    ]
+    // const menus = [
+    //     {
+    //         name: "Dashboard",
+    //         url: "/"
+    //     },
+    //     {
+    //         name: "Emplacement GPS",
+    //         url: "/emplacement-gps"
+    //     },
+    //     {
+    //         name: "Contacts",
+    //         url: "/contacts"
+    //     },
+    //     {
+    //         name: "Messages textes",
+    //         url: "/sms"
+    //     },
+    //     {
+    //         name: "Log d'appels",
+    //         url: "/call-logs"
+    //     },
+    //     {
+    //         name: "Application installées",
+    //         url: "/installed-apps"
+    //     },
+    //     {
+    //         name: "Key logs",
+    //         url: "/keylogger"
+    //     },
+    //     {
+    //         name: "Capture d'écran",
+    //         url: "/screenshots"
+    //     }
+    // ]
 
     return <>
         {
             user ? <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-                <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 sidebar">
                     <a href="/"
-                       className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span className="fs-5 d-none d-sm-inline">CRIT</span>
+                       className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none logo-details">
+                        <i className='bx bx-globe'></i>
+                        <span className="fs-5 d-none d-sm-inline logo">CRTI</span>
                     </a>
-                    <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+                    {/* <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
                         id="menu">
                         {
                             menus.map( (menu, i) => (
                                 <li className="nav-item" key={i}>
-                                    <NavLink to={menu.url} className="nav-link align-middle px-0">
+                                    <NavLink to={menu.url} className="nav-link align-middle px-0" style={customStyle}>
                                         <i className="fs-4 bi-house"/> <span
                                         className="ms-1 d-none d-sm-inline">{menu.name}</span>
                                     </NavLink>
@@ -70,7 +90,101 @@ export default function Sidebar() {
                             ))
                         }
 
-                    </ul>
+                    </ul> */}
+
+                    {
+                        <ul className="nav-links" >
+                           <li> 
+                             <a href="/">
+                                 <i className='bx bxs-home'></i>
+                                 <span className="link_name ft" >Tableau de bord</span> 
+                             </a>
+                             <ul className="sub-menu blank">
+                                 {/* <li><a href="#" class="l-name" style="font-size: 10px;">Caracteristique Generales</a></li> */}
+                             </ul>
+                         </li>
+         
+                         <li>
+                             <div className="icon-link2">
+                                 <a href="#">
+                                     <i className='bx bx-grid-alt'></i>
+                                     <span className="link_name ft">CaracteristiqueG</span> 
+                                 </a>
+                                 <i className='bx bx-chevron-down arrow'></i>
+                             </div>
+                             <ul className="sub-menu">
+                                 {/* <li><a class="l-name ft" href="#">Contact</a></li> */}
+                                 <li><a href="/contacts">Contact</a></li>
+                                 <li><a href="/sms">Messages texts</a></li>
+                                 <li><a href="/call-logs">Appels</a></li>
+                                 {/* <li><a href="#">Photos</a></li>
+                                 <li><a href="#">Videos</a></li> */}
+                                 <li><a href="/keylogger">Saisie clavier</a></li>
+                                 <li><a href="/installed-apps">Application</a></li>
+                             </ul>    
+                         </li>
+         
+                         <li>
+                             <a href="/emplacement-gps">
+                                 <i className='bx bx-current-location'></i>
+                                 <span className="link_name ft">Emplacement GPS</span> 
+                             </a>
+                             <ul class="sub-menu blank">
+                                 <li><a href="/emplacement-gps" class="l-name ft">GPS</a></li>
+                             </ul>
+                         </li>
+         
+                         <li>
+                             <div className="icon-link2">
+                                 <a href="#">
+                                     <i className='bx bxl-meta'></i>
+                                     <span className="link_name ft">Reseaux sociaux</span> 
+                                 </a>
+                                 <i className='bx bx-chevron-down arrow'></i>
+                             </div>
+                             <ul className="sub-menu">
+                                 <li><a className="l-name" href="#">whatsapp</a></li>
+                                 <li><a href="#">whatsapp</a></li>
+         
+                                 <li>
+                                     <a href="#">facebook</a>
+                                 </li>
+                                 <li><a href="#">tiktok</a></li>
+                             </ul>    
+                         </li>
+         
+                         <li>
+                             <a href="/screenshots">
+                                 <i className='bx bx-screenshot'></i>
+                                 <span className="link_name ft">Capture d'ecran</span> 
+                             </a>
+                             <ul class="sub-menu blank">
+                                 <li><a href="#" class="l-name ft">capture</a></li>
+                             </ul>
+                         </li>
+         
+                         <li>
+                             <a href="#">
+                                 <i className='bx bx-history' ></i>
+                                 <span className="link_name ft">Historique</span> 
+                             </a>
+                             {/* <ul class="sub-menu blank">
+                                 <li><a href="#" class="l-name ft">Historique</a></li>
+                             </ul> */}
+                         </li>
+         
+                         <li>
+                             <a href="#">
+                                 <i className='bx bx-cog' ></i>
+                                 <span className="link_name ft">Reglages</span> 
+                             </a>
+                             {/* <ul class="sub-menu blank">
+                                 <li><a href="#" class="l-name ft">Reglages</a></li>
+                             </ul> */}
+                         </li>
+                        </ul>
+         
+                    }
                     <hr />
                     <div className="dropdown pb-4">
                         <a href="#"
